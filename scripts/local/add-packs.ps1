5ad96c39354ce6dfa6a9609c062c9becc6e87e8a
Nick Atkins <nbatkins@gmail.com>
2022-08-01 00:45:00 -0500

    add tech reborn

---
+++ scripts/local/add-packs.ps1
@@ -0,0 +1,18 @@
+pk mr add ae2
+pk mr add can-you-trash-it
+pk mr add modern-industrialization
+pk mr add applied-energistics-2-wireless-terminals
+pk mr add botania
+pk cf add applied-botanics-addon
+pk mr add modern-industrialization
+pk mr add upc
+pk cf add techreborn
+pk cf add quarry-reborn
+pk cf add advancedreborn
+pk cf add gearreborn
+pk cf add upc
+pk cf add tech-reborn-patchouli
+pk refresh
+git add --all
+git commit -m 'add tech reborn'
+git push
